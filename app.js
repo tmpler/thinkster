b44e4f47e450465d973281d061205efeaf668fa8
Monarchi <mon@rchi.uk>
2016-03-30 16:36:18 +0000

    comments

--- app.js
+++ app.js
@@ -28,11 +28,27 @@ app.factory('posts', [function(){
   return o;
 }]);
 
-app.controller('MainCtrl', [
+app.controller('PostsCtrl', [
 '$scope',
 '$stateParams',
 'posts',
 function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
+}]);
+
+app.controller('MainCtrl', [
+'$scope',
+'posts',
+function($scope, posts){
   $scope.posts = posts.posts;
   $scope.incrementUpvotes = function(post) {
     post.upvotes += 1;
@@ -50,7 +66,11 @@ function($scope, $stateParams, posts){
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]
     });
     $scope.title = '';
     $scope.link = '';
